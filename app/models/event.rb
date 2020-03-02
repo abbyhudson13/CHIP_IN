126771455601c08b5b97088cf2a8d430bc07da60
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-03-02 17:43:59 +0100

    adding models

---
+++ app/models/event.rb
@@ -0,0 +1,5 @@
+class Event < ApplicationRecord
+  belongs_to :user
+  has_many :requests
+  has_many :users, through: :requests
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_02_163851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.text "description"
+    t.date "starts_at"
+    t.date "ends_at"
+    t.integer "capacity"
+    t.string "category"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.integer "status"
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_requests_on_event_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.text "bio"
+    t.text "address"
+    t.string "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "events", "users"
+  add_foreign_key "requests", "events"
+  add_foreign_key "requests", "users"
 end
