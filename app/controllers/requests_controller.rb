d422220c8942ac9e3d586c7e9f445f24d3b7f20d
Miguel Castro <quecontentoestoy@gmail.com>
2020-03-03 11:43:41 +0100

    create request controller

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,10 @@
+class RequestsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'requests/new'
+  get 'requests/create'
+  get 'requests/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :events do
