46144b88628b1dbf4a39d394ddae016ebdc1d3ce
Miguel Castro <quecontentoestoy@gmail.com>
2020-03-03 15:31:11 +0100

    added status options and validations for the request in the request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,12 @@
+STATUS = ['1', '2', '3','4']
+validates_
+
 class Request < ApplicationRecord
+  validates :status, inclusion: {in: [1,2,3,4]}
+end
+
   belongs_to :user
   belongs_to :event
+
+
 end
