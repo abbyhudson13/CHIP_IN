1e55aceb95ad9924314ed4b1653d42fa0db8ba8c
Miguel Castro <quecontentoestoy@gmail.com>
2020-03-03 12:40:13 +0100

    added new, create, edit, update, destroy and private methods

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,52 @@
 class RequestsController < ApplicationController
+  before_action :set_event, only: [:new, :create]
+
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(request_params)
+    @request.event = @event
+    @request.user = current_user
+    @request.status = 1
+    if @request.save
+       redirect_to root_path
+       # need to be change when dashboard is available
+     else
+      render :new
+    end
   end
 
-  def destroy
+  def edit
+    @request = Request.find(params[:id])
   end
+
+  def update
+
+    @request = Request.find(params[:id])
+    @request.update(request_params)
+    redirect_ root_path
+    redirect_to event_path(@event)
+  end
+
+
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to root_path
+    # need to be change when dashboard is available
+
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:message)
+  end
+  def set_event
+    @event = Event.find(params[:event_id])
+
+  end
+
 end
