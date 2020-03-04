91529e5e71f32b5e1162c8ba64008b8d2233b3a1
Mariia Kahlert <mariakahlert@icloud.com>
2020-03-04 13:05:14 +0100

    Adds datetime user-friendlier format on the event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,15 @@
-CATEGORIES = ["Poverty", "Women & Families", "Refugees", "Children", "Animals", "Environment", "Elderly", "Education"]
-
 class Event < ApplicationRecord
+  CATEGORIES = ["Poverty", "Women & Families", "Refugees", "Children", "Animals", "Environment", "Elderly", "Education"]
   belongs_to :user
   has_many :requests
   has_many :users, through: :requests
   has_one_attached :photo
+
+  def starts_at_formatted
+    starts_at.strftime('%A, %d %b %Y %l:%M %p')
+  end
+
+  def ends_at_formatted
+    ends_at.strftime('%A, %d %b %Y %l:%M %p')
+  end
 end
