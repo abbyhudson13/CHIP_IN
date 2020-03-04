f84c26b9cb45cfb19471d8f758aff83e3a24b73b
Ben Chavez <ben.m.chavez@gmail.com>
2020-03-04 13:27:12 +0100

    I styled the landing page with a baseline  structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :requests, dependent: :destroy
   has_many :events, dependent: :destroy
   has_one_attached :photo
-  validates :photo, attached: true
+  # validates :photo, attached: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
