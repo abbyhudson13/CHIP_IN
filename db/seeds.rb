9200ce06e2a9843be923c191383bad75c5e0654e
Miguel Castro <quecontentoestoy@gmail.com>
2020-03-05 12:50:19 +0100

    seedsbreaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ file = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/C
 
 youngcaritas = User.create(
   username:"Youngcaritas Berlin",
-  password: "1234556",
+  password: "123456",
   email:"youngcaritas@caritas-berlin.de",
   address: "Residenzstraße 90 13409 Berlin",
   bio:"Youngcaritas Berlin is part of the Caritas Association for the Archdiocese of Berlin and promotes young commitment. Our aim is to introduce young people to politically and socially controversial issues and to make them more aware of environmental resources through both theoretical input and practical implementation. The aim of youngcaritas Berlin is to attract as many young people as possible to volunteer, be it for the youngcaritas projects Kulturbuddys, vergissmeinnicht* or through advice and placement in various institutions and projects.",
@@ -34,7 +34,7 @@ file1 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 servethecitiy = User.create(
   username:"Serve the City",
-  password: "1234556",
+  password: "123456",
   email:"christine@servethecity.berlin",
   address: "Greifenhagener Str. 60 10437 Berlin",
   bio:"STC Berlin e. V. mobilizes volunteers to show an act of kindness in a practical way. In practical terms, we do this by painting walls, serving food, visiting people, playing games, making music, organizing sports festivals, through art and creativity, collecting garbage and much more. We serve children, youth and adults, older people, people with disabilities, homeless people and other people who are on the margins of society or in need of help.",
@@ -52,7 +52,7 @@ file2 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 berlinerobdachlosen = User.create(
   username:"Berliner Obdachlosen Hilfe",
-  password: "1234556",
+  password: "123456",
   email:"kontakt@berliner-obdachlosenhilfe.de",
   address: "Lynarstraße 38 13353 Berlin",
   bio:"Berliner Obdachlosenhilfe is a non-profit organisation run by volunteers that help people who lost their homes and live on the street.",
@@ -70,7 +70,7 @@ file3 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 familienzentrum = User.create(
   username:"Familenzentrum-Adalbert",
-  password: "1234556",
+  password: "123456",
   email:"familienzentrum@jugendwohnen-berlin.de",
   address: "Adalbertstr. 23b 10997 Berlin",
   bio:"Jugendwohnen im Kiez offers help from a single source in the youth and social sector. Specifically, we develop and implement offers in the fields of work: child and youth welfare, cooperation youth welfare school, education networks, family centres, day care centres and district management.",
@@ -86,7 +86,7 @@ file4 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 junkfood = User.create(
   username:"Junk Food Project",
-  password: "1234556",
+  password: "123456",
   email:"realjunkfoodberlin@yahoo.com",
   address: "Bezirk Mitte, 13359 Berlin",
   bio:"We want to help to reduce food waste by turning surplus fruit and veg into delicious delicatessen and healthy meals that are provided to the public and other social projects and events (usually connected with sustainability and/or social issues). We bring people together and share the joy of cooking and eating together while serving a great cause of preventing food waste.",
@@ -102,7 +102,7 @@ file5 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 verbundtreptow = User.create(
   username:"Psychosozialer Verbund Treptow e.V",
-  password: "1234556",
+  password: "123456",
   email:"info@psv-treptow.de",
   address: "Kiefholzstr. 414 12435 Berlin",
   bio:"We offer assisted living for mentally ill and mentally handycapped people. Also, we have an employment project in our Café Grenzenlos for people being excluded from work at the job market due to their handicaps.",
@@ -119,7 +119,7 @@ file6 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 speak = User.create(
   username:"SPEAK",
-  password: "1234556",
+  password: "123456",
   email:"speak@speak.com",
   address: "Schillingstraße 12 10179 Berlin",
   bio:"SPEAK is a social startup that connects migrants, refugees and locals living in the same city. This happens through informal and dynamic language groups  and events that are open to everyone in the community! Anyone can register to be a buddy to help others learn a language and share their culture while meeting incredible people.",
@@ -136,7 +136,7 @@ file7 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 dwb = User.create(
   username:"Diakonisches Werk Berlin e.V",
-  password: "1234556",
+  password: "123456",
   email:"r.fu@diakonie-stadtmitte.de",
   address: "Zeughofstraße 12 10997 Berlin",
   bio:"The refugee shelter is located in a quite back street in the heart of Berlin Kreuzberg. Up to 147 women, men and children from all over the world live here. Most of them are war refugees or asylum seekers who can not return to their home countries for various reasons. We offer social counselling and support regarding administrative and bureaucratic matters and help to find German classes. ",
@@ -153,7 +153,7 @@ file8 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 berlinerstadtmission = User.create(
   username:"Berliner Stadtmission",
-  password: "1234556",
+  password: "123456",
   email:"info@berliner-stadtmission.de",
   address: "Lehrter Str. 68 10557 Berlin",
   bio:"'Berliner Stadtmission' is an independent institution under the guardianship of the protestant church Berlin-Brandenburg-schlesische Oberlausitz.",
@@ -169,7 +169,7 @@ berlinerstadtmission.photo.attach(io:file8, filename:'logo8.png', content_type:'
 file9 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/CHIP-IN/Charities%20pics/Ocean-now_pllid5.jpg')
 oceannow = User.create(
   username:"Ocean Now",
-  password: "1234556",
+  password: "123456",
   email:"info@ocean-now.org",
   address: "Danneckerstr. 8 10245 Berlin",
   bio:"Ocean. Now!  is an NGO accelerating ocean protection on all levels of society. We work with art. The “Sustainable Development Goal 14 (SDG 14)” of the Paris Climate Agreement is the fraame of our work.",
@@ -186,7 +186,7 @@ file10= URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321092/
 
 unionhilfwerk = User.create(
   username:"Unionhilfswerk",
-  password: "1234556",
+  password: "123456",
   email:"dialog@unionhilfswerk.de",
   address: "Mühsamstraße 33 10249 Berlin",
   bio:"Since 1946, the UNIONHILFSWERK has been providing social aid out of Christian responsibility. To this day, we remain committed to those who depend on care and support. Our commitment is shaped by our mission statement 'to create an individual quality of life' in order to help people to help themselves. Originating from 70 years of civic involvement, voluntary involvement continues to be a fundamental pillar of our commitment to others.",
@@ -203,7 +203,7 @@ file11= URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 casablanca = User.create(
   username:"Casablanca mbH",
-  password: "1234556",
+  password: "123456",
   email:"emueller@g-casablanca.de",
   address: "Pistoriusstraße 108 a 13086 Berlin",
   bio:"Casablanca — a charitable society for innovative youth welfare services and social services mbH — has been devoted to using targeted welfare solutions to support children, young people and families in difficult life situations since 1995",
@@ -220,7 +220,7 @@ file12= URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091/
 
 keinabseits = User.create(
   username:"Kein Abseits! e.V",
-  password: "1234556",
+  password: "123456",
   email:"info@kein-abseits.de",
   address: "Fehmarner Straße 12 13353 Berlin",
   bio:"The non-profit association 'kein Abseits! e.V.' realizes integration and education projects since 2011. In cooperation with Berlin schools, universities and refugee homes, we pursue the goal of enabling integrative encounters. Local and refugee children and adolescents are individually and holistically supported in a combination of sports and experiential educational offers, 1: 1 mentoring and vocational orientation.",
@@ -237,7 +237,7 @@ file13 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321091
 
 solinar = User.create(
   username:"SoliNaR e.V",
-  password: "1234556",
+  password: "123456",
   email:"info@kein-abseits.de",
   address: "Fehmarner Straße 12 13353 Berlin",
   bio:"The non-profit association 'kein Abseits! e.V.' realizes integration and education projects since 2011. In cooperation with Berlin schools, universities and refugee homes, we pursue the goal of enabling integrative encounters. Local and refugee children and adolescents are individually and holistically supported in a combination of sports and experiential educational offers, 1: 1 mentoring and vocational orientation.",
@@ -253,7 +253,7 @@ file14 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583321092
 
 stepsforpeace = User.create(
   username:"Steps For Peace",
-  password: "1234556",
+  password: "123456",
   email:"info@steps-for-peace.org",
   address: "Hochkirchstraße 7 10829 Berlin",
   bio:"Steps for Peace  is a nonprofit organisation based in Berlin that supports social and political transformation processes in fragile contexts. Our work focuses on education, research and development cooperation.",
@@ -268,13 +268,8 @@ stepsforpeace.photo.attach(io:file14, filename: 'logo14.jpg', content_type: 'ima
 
 #####################################
 
-
-require "open-uri"
-
-User.destroy_all
-
-# volunteer 1
-file = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402819/CHIP-IN/Volunteers/Luis_t3um21.jpg')
+#volunteer 1
+file15 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583408178/CHIP-IN/Volunteers/Luis_ofn4ym.jpg')
 
  luisito = User.create(
   username:"topdesigner",
@@ -287,11 +282,11 @@ file = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402819/C
   webpage: "http://www.luisitotopprogrammer.com"
 )
 
-luisito.photo.attach(io:file, filename:'logo.jpg', content_type: 'image/jpg')
+luisito.photo.attach(io:file15, filename:'logo15.jpg', content_type: 'image/jpg')
 
 
 # volunteer 2
-file1 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583334950/CHIP-IN/Volunteers/Daniel_qn850x.png')
+file16 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583408178/CHIP-IN/Volunteers/Daniel_fkbxo3.png')
 
  daniel = User.create(
   username:"greekgod",
@@ -304,10 +299,10 @@ file1 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583334950/
   webpage: "http://www.malaka.com"
 )
 
-daniel.photo.attach(io:file1, filename:'logo1.png', content_type: 'image/png')
+daniel.photo.attach(io:file16, filename:'logo16.png', content_type: 'image/png')
 
 # volunteer 3
-file2 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402586/CHIP-IN/Volunteers/jean_pfvfdm.jpg')
+file17 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583408177/CHIP-IN/Volunteers/jean_ojoatf.jpg')
 
  jean = User.create(
   username: "bestdj",
@@ -320,11 +315,11 @@ file2 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402586/
   webpage: "http://www.bestdjinberlin.com"
 )
 
-jean.photo.attach(io:file2, filename:'logo2.jpg', content_type: 'image/jpg')
+jean.photo.attach(io:file17, filename:'logo17.jpg', content_type: 'image/jpg')
 
 #volunteer 4
 
-file3 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402666/CHIP-IN/Volunteers/claire_s9sxz4.jpg')
+file18 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583408177/CHIP-IN/Volunteers/claire_ujuzpd.jpg')
 
  claire = User.create(
   username: "clairetheTA",
@@ -337,11 +332,11 @@ file3 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402666/
   webpage: "http://www.bestTAlewagon.com"
 )
 
-claire.photo.attach(io:file3, filename:'logo3.jpg', content_type: 'image/jpg')
+claire.photo.attach(io:file18, filename:'logo18.jpg', content_type: 'image/jpg')
 
 #volunteer 5
 
-file4 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402446/CHIP-IN/Volunteers/Tim_qoabmr.jpg')
+file19 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583408180/CHIP-IN/Volunteers/Tim_xtredv.jpg')
 
  tim = User.create(
   username: "Timsitopapito",
@@ -354,11 +349,11 @@ file4 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583402446/
   webpage: "http://www.bestTAlewagon.com"
 )
 
-tim.photo.attach(io:file4, filename:'logo4.jpg', content_type: 'image/jpg')
+tim.photo.attach(io:file19, filename:'logo19.jpg', content_type: 'image/jpg')
 
 #volunteer 6
 
-file5 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583404421/CHIP-IN/Volunteers/55158466_vswfwr.jpg')
+file20 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583408177/CHIP-IN/Volunteers/55158466_ghxdgi.jpg')
 
  abby = User.create(
   username: "Abby",
@@ -371,22 +366,5 @@ file5 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583404421/
   webpage: "http://www.alwaysraininengland.com"
 )
 
-abby.photo.attach(io:file5, filename:'logo5.jpg', content_type: 'image/jpg')
+abby.photo.attach(io:file20, filename:'logo20.jpg', content_type: 'image/jpg')
 
-
-#volunteer 7
-
-file5 = URI.open('https://res.cloudinary.com/dqlvehu5z/image/upload/v1583404421/CHIP-IN/Volunteers/55158466_vswfwr.jpg')
-
- abby = User.create(
-  username: "Abby",
-  password: "1234556",
-  email: "abbyabby@lewagon.com",
-  address: "Brandenburgische Straße 52 12099 Berlin",
-  bio: "I worked for some large consumer goods companies in commercial roles. I realised how fun is to have contact with other people and help others. Want to make a difference in my community",
-  full_name: "Abby Hudson",
-  role: "Volunteer",
-  webpage: "http://www.alwaysraininengland.com"
-)
-
-abby.photo.attach(io:file5, filename:'logo5.jpg', content_type: 'image/jpg')
