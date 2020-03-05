a3b5c99936f3ec1064b8e4043733d3893696c509
Mariia Kahlert <mariakahlert@icloud.com>
2020-03-05 15:07:33 +0100

    Changes role data type to integer so that enum logic works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_161831) do
+ActiveRecord::Schema.define(version: 2020_03_05_135829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,9 +74,9 @@ ActiveRecord::Schema.define(version: 2020_03_04_161831) do
     t.string "username"
     t.text "bio"
     t.text "address"
-    t.string "role"
     t.string "full_name"
     t.string "webpage"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
