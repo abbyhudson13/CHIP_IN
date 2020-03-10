e2efbdc62341bc7c6de182225b6e830c54f15f4a
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-03-10 15:21:20 +0100

    amending the cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_10_112852) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,8 +87,8 @@ ActiveRecord::Schema.define(version: 2020_03_10_112852) do
     t.text "address"
     t.string "full_name"
     t.string "webpage"
-    t.integer "notifications", default: 0
     t.integer "role", default: 1
+    t.integer "notifications", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
